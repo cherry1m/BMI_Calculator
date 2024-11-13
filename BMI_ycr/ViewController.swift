f1986be0d51203fc99f7057128c3e4a729fd04eb
cherry1m <ini_045@naver.com>
2024-11-13 16:45:06 +0900

    Initial Commit

---
+++ BMI_ycr/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI_ycr
+//
+//  Created by 윤채림 on 11/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
