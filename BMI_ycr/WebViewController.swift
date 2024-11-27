557fb35d2241da3de5defaebeee377f3280a7258
cherry1m <ini_045@naver.com>
2024-11-27 16:47:24 +0900

    videoViewController 완성 및 WebViewController 를 생성하여 유튜브와 블로그 주소를 연동하는 코드를 작성하였습니다.

--- BMI_ycr/WebViewController.swift
+++ BMI_ycr/WebViewController.swift
@@ -1,5 +1,5 @@
 //
-//  VideoViewController.swift
+//  WebViewController.swift
 //  BMI_ycr
 //
 //  Created by 윤채림 on 11/27/24.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class VideoViewController: UIViewController {
+class WebViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
